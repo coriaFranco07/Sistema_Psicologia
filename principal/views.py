abb44c0c555ce52a8aa925f1cfc95b73637af1a2
Franco Coria Zaragoza <139906007+coriaFranco07@users.noreply.github.com>
2026-05-09 11:51:55 -0300

    Redirect staff users to admin panel after login

--- principal/views.py
+++ principal/views.py
@@ -1,4 +1,7 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView
 
 from .forms import LoginForm
 
@@ -8,6 +11,12 @@ class StyledLoginView(LoginView):
     authentication_form = LoginForm
     redirect_authenticated_user = False
 
+    def get_success_url(self):
+        user = self.request.user
+        if user.is_staff or user.is_superuser:
+            return reverse_lazy("panel_admin")
+        return super().get_success_url()
+
     def form_valid(self, form):
         response = super().form_valid(form)
         if form.cleaned_data.get("remember_me"):
@@ -15,3 +24,11 @@ class StyledLoginView(LoginView):
         else:
             self.request.session.set_expiry(0)
         return response
+
+
+class PanelAdminView(LoginRequiredMixin, UserPassesTestMixin, TemplateView):
+    template_name = "panel_admin.html"
+
+    def test_func(self):
+        user = self.request.user
+        return user.is_staff or user.is_superuser
